0b9914cca02b9242e06a28f0c50eece101bbf8bb
zehfernandes <ozehfernandes@gmail.com>
2019-09-29 18:11:37 -0300

    init

--- resources/webview.js
+++ resources/webview.js
@@ -1,14 +1,34 @@
+//Get Libraries
+const libraries = [];
+const breakpoints = [];
+
 // disable the context menu (eg. the right click menu) to have a more native feel
-document.addEventListener('contextmenu', (e) => {
-  e.preventDefault()
-})
+document.addEventListener("contextmenu", e => {
+  e.preventDefault();
+});
 
 // call the plugin from the webview
-document.getElementById('button').addEventListener('click', () => {
-  window.postMessage('nativeLog', 'Called from the webview')
-})
+document.getElementById("button").addEventListener("click", () => {
+  const libsElm = document.querySelectorAll(".libField");
+  const breakNameElm = document.querySelectorAll(".breakpointNameField");
+  const breakSizeElm = document.querySelectorAll(".breakpointSizeField");
+
+  document.forEach((el, i) => {
+    libraries.push(el.value);
+  });
+
+  breakNameElm.forEach((el, i) => {
+    breakpoints.push({
+      name: el.value,
+      size: breakSizeElm[i].value
+    });
+  });
+
+  window.postMessage("savePreferences", { libraries, breakpoints });
+});
 
 // call the wevbiew from the plugin
-window.setRandomNumber = (randomNumber) => {
-  document.getElementById('answer').innerHTML = 'Random number from the plugin: ' + randomNumber
-}
+window.loadConfigs = configs => {
+  const { libraries, breakpoints } = configs;
+  //document.getElementById("answer").innerHTML = "Random number from the plugin: " + randomNumber;
+};
